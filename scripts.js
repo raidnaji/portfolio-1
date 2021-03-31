186826feb8ecc89c965ff95945231f937d668200
antonmontrezor <montrezoranton991@gmail.com>
2021-03-31 16:52:09 -0400

    started working on responsive design, wrote logic to animate project section elements differently within smaller screen sizes

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const app = {};
 app.displayNav = () => {
     const navigationMenu = document.querySelector('.mainNav');
 
-        if (window.pageYOffset > 500) {
+        if (window.pageYOffset > window.innerHeight * 0.7) {
             navigationMenu.classList.add('visible');
             navigationMenu.classList.remove('hidden');
             
@@ -13,8 +13,49 @@ app.displayNav = () => {
         }
 }
 
+app.changeAnimation = () => {
+    const elementsAnimatedLeft = document.querySelectorAll('[data-aos="fade-left"]');
+    const elementsAnimatedRight = document.querySelectorAll('[data-aos="fade-right"]');
+    // const soccerImage = document.querySelector('#soccerImage');
+    // const 
+    
+    for (let element of elementsAnimatedLeft) {
+        if (window.innerWidth < 1000) {
+            console.log(element.classList[0] !== 'imageContainer')
+            if (element.classList[0] !== 'imageContainer') {
+                element.removeAttribute('data-aos');
+                element.setAttribute('data-aos', 'fade-up');
+            } else {
+                element.removeAttribute('data-aos');
+                element.setAttribute('data-aos', 'fade-down');
+            }
+        } else {
+            element.removeAttribute('data-aos');
+            element.setAttribute('data-aos', 'fade-left');
+        }
+    } 
+
+    for (let element of elementsAnimatedRight) {
+        if (window.innerWidth < 1000) {
+            console.log(element.classList[0] !== 'textContent')
+            if (element.classList[0] !== 'textContent') {
+                element.removeAttribute('data-aos');
+                element.setAttribute('data-aos', 'fade-down'); 
+            } else {
+                element.removeAttribute('data-aos');
+                element.setAttribute('data-aos', 'fade-up');
+            }
+            
+        } else {
+            element.removeAttribute('data-aos');
+            element.setAttribute('data-aos', 'fade-right');
+        }
+    } 
+}
+
 app.appInit = () => {
     window.addEventListener("scroll", app.displayNav);
+    window.addEventListener("DOMContentLoaded", app.changeAnimation);
 }
 
 app.appInit();
